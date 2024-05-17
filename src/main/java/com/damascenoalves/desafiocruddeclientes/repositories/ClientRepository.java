cd3cacb244f02599fc797fa65c9ec8ee98950153
Matheus Damasceno Alves <matheusd.alves@hotmail.com>
2024-05-17 11:59:01 -0300

    Client repository

---
+++ src/main/java/com/damascenoalves/desafiocruddeclientes/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.damascenoalves.desafiocruddeclientes.repositories;
+
+import com.damascenoalves.desafiocruddeclientes.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
