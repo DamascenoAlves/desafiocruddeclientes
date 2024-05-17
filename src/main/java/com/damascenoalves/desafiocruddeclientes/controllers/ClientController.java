416a753f8db27b66814a5a292168c4adc6da3965
Matheus Damasceno Alves <matheusd.alves@hotmail.com>
2024-05-17 13:21:13 -0300

    GET Client by id

--- src/main/java/com/damascenoalves/desafiocruddeclientes/controllers/ClientController.java
+++ src/main/java/com/damascenoalves/desafiocruddeclientes/controllers/ClientController.java
@@ -1,8 +1,12 @@
 package com.damascenoalves.desafiocruddeclientes.controllers;
 
 
+import com.damascenoalves.desafiocruddeclientes.dto.ClientDTO;
 import com.damascenoalves.desafiocruddeclientes.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,10 @@ public class ClientController {
 
     @Autowired
     private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
 }
--- src/main/java/com/damascenoalves/desafiocruddeclientes/services/ClientService.java
+++ src/main/java/com/damascenoalves/desafiocruddeclientes/services/ClientService.java
@@ -1,14 +1,23 @@
 package com.damascenoalves.desafiocruddeclientes.services;
 
 
+import com.damascenoalves.desafiocruddeclientes.dto.ClientDTO;
 import com.damascenoalves.desafiocruddeclientes.repositories.ClientRepository;
+import com.damascenoalves.desafiocruddeclientes.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ClientService {
 
     @Autowired
     private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+        ClientDTO dto = new ClientDTO(repository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Recurso nao encontrado")));
+        return dto;
+    }
 
 }
