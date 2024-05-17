aa2104bfd41a7e71808d34580290fa8be876a204
Matheus Damasceno Alves <matheusd.alves@hotmail.com>
2024-05-17 12:00:52 -0300

    ClientService

---
+++ src/main/java/com/damascenoalves/desafiocruddeclientes/services/ClientService.java
@@ -0,0 +1,14 @@
+package com.damascenoalves.desafiocruddeclientes.services;
+
+
+import com.damascenoalves.desafiocruddeclientes.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+    
+}
